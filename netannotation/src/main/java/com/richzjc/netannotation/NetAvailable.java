2b8b6dc61115bc41211e73ca4b896c817872ccbb
richzjc <zhangjianchuan@wallstreetcn.com>
2020-06-03 19:13:50 +0800

    add files

--- netannotation/src/main/java/com/richzjc/netannotation/NetAvailable.java
+++ netannotation/src/main/java/com/richzjc/netannotation/NetAvailable.java
@@ -6,6 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.CLASS)
-@Target(ElementType.TYPE)
+@Target(ElementType.METHOD)
 public @interface NetAvailable {
 }
